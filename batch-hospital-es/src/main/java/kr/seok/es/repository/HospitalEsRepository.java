9ce26728472b6c481877ca0d3dd89378446132f3
seokrae <kslbsh@gmail.com>
2021-07-03 23:45:29 +0900

    refactor: es 엔티티 테스트

--- batch-hospital-es/src/main/java/kr/seok/es/repository/HospitalEsRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.seok.es.repository;
-
-
-import kr.seok.es.domain.HospitalEsEntity;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface HospitalEsRepository extends ElasticsearchRepository<HospitalEsEntity, String> {
-
-}
